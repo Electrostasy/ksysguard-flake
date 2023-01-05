277a80f5c04424c113814b8099a6764a0e93874c
Gediminas Valys <steamykins@gmail.com>
2023-01-05 21:14:35 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Last working Nixpkgs revision for Ksysguard";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/60632718c063ad4d1bd1e2b82ba2f490fa64bbd8";
+  
+  outputs = {
+    self,
+    nixpkgs
+  }:
+  let
+    inherit (nixpkgs) lib;
+  in
+  {
+    packages =
+      lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system:
+        lib.fix self.overlays.default nixpkgs.legacyPackages.${system});
+
+    overlays.default = final: prev: {
+      ksysguard = prev.ksysguard.overrideAttrs (old: {
+        postInstall = ''
+          ${prev.libcap}/bin/setcap "cap_net_raw+ep" "$out/bin/ksysguard"
+        '';
+      });
+    };
+  };
+}
