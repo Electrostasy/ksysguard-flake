6ff8e936815aa9425b7625ebde8ff8b724aaab7b
Gediminas Valys <steamykins@gmail.com>
2023-01-06 19:43:56 +0200

    update again again

--- flake.nix
+++ flake.nix
@@ -1,11 +1,18 @@
 {
   description = "Last working Nixpkgs revision for Ksysguard";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/60632718c063ad4d1bd1e2b82ba2f490fa64bbd8";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/60632718c063ad4d1bd1e2b82ba2f490fa64bbd8";
+    nix-utils = {
+      url = "github:juliosueiras-nix/nix-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
   
   outputs = {
     self,
-    nixpkgs
+    nixpkgs,
+    nix-utils
   }:
   let
     inherit (nixpkgs) lib;
@@ -16,14 +23,25 @@
         lib.fix self.overlays.default nixpkgs.legacyPackages.${system});
 
     overlays.default = final: prev: {
-      # For convenience
       inherit (prev.plasma5Packages) libksysguard;
 
-      ksysguard-standalone = prev.ksysguard.overrideAttrs (old: {
-        postFixup = ''
-          ${prev.libcap}/bin/setcap "cap_net_raw+ep" "$out/libexec/ksysguard/.ksgrd_network_helper-wrapped"
-        '';
-      });
+      ksysguard-standalone =
+        let
+          bundle = nix-utils.bundlers.deb {
+            inherit (prev) system;
+            program = prev.ksysguard.outPath;
+          };
+        in
+          bundle.overrideAttrs (old: {
+            postInstall =
+              let
+                debPostInstall = prev.writeShellScript "after-install.sh" ''
+                  ${prev.libcap}/bin/set/cap "cap_net_raw+ep" "$out/libexec/ksysguard/.ksgrd_network_helper-wrapped"
+                '';
+              in ''
+                ${prev.fpm}/bin/fpm -s deb -t dev --name ksysguard-standalone --after-install ${debPostInstall} "$out/*.deb"
+              '';
+          });
     };
   };
 }
